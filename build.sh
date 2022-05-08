54d8fa4efd4a59a1c721ee211006710cf0c484d1
tiennm <tiennm1@galaxy.com.vn>
2022-05-08 13:59:01 +0700

    Update

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 export VERSION=$(cat package.json | grep version | head -1 | awk -F= "{ print $2 }" | sed 's/[version:,\",]//g' | tr -d '[[:space:]]') &&   
-export IMAGE_NAME=nmcnpmg1/ccnlthd-backend &&
-docker build -t $IMAGE_NAME:latest . &&
-docker login -u nmcnpmg1 -p nmcnpmg123 && 
+export IMAGE_NAME=tienvnz98/reverse-proxy &&
+docker build -t $IMAGE_NAME:latest . && 
 docker push $IMAGE_NAME:latest &&
 docker build -t $IMAGE_NAME:$VERSION . &&
 docker push $IMAGE_NAME:$VERSION &&
